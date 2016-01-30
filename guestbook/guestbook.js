7a4f96db0ed97063d69c3dbf729b6bf9f4ec34f0
sdemecilio <stacey.demecilio@live.com>
2016-01-30 02:10:13 -0800

    Basic version that works for guestbook functions.

--- guestbook/guestbook.js
+++ guestbook/guestbook.js
@@ -1,23 +1,38 @@
-if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
+Messages = new Mongo.Collection('messages');
 
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
-    }
-  });
-
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
-    }
-  });
+if (Meteor.isClient)
+{
+	Meteor.subscribe("messages");
+	
+	Template.guestBook.events({});
+	
+	Template.guestBook.events
+	({
+		'submit form':function(event)
+		{
+			event.preventDefault();
+			var messageBox = $(event.target).find('textarea[name=guestBookMessage]');
+			var messageText = messageBox.val();
+			var nameBox = $(event.target).find('input[name=guestName]');
+			var name = nameBox.val();
+			
+			Messages.insert({message: messageText, name: name, createdOn: Date.now()});
+		}
+	});
+	
+	Template.guestBook.helpers({
+		'messages':function()
+		{
+			return Messages.find({}, {sort: {createdOn: -1}}) || {};
+		}
+	});
 }
 
-if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
-  });
+if (Meteor.isServer)
+{
+	// this code only runs on the server
+	Meteor.publish("messages", function()
+	{
+		return Messages.find();
+	});
 }
